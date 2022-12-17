d1e84f4d38d7d176f6d786785ae02beeaa38198e
RomPop76 <r.popielas@gmail.com>
2022-12-17 07:16:32 +0100

    Commit 3

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
-
+$(document).on('scroll', function() {         //nasłuchiwanie na scrolla
+    const scrollValue = $(this).scrollTop()   //określenie pozycji scrolla od góry
+    const $art2 = $('.art2');                 //pobranie art2
+    const art2FromTop = $art2.offset().top;   //odległość ar2 od początku
+    const art2Height = $art2.outerHeight()    //wysokość atr2
+    
+    
+})
